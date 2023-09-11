c39914df341dce77ed365356cf0668a9867c6ead
Frank Zijlstra <22915457+FrankZijlstra@users.noreply.github.com>
2023-09-11 13:34:10 +0200

    Several bug fixes in `Sequence`, `add_gradients`, `make_trapezoid`, and `rotate`: - Bypass code for `restoreAdditionalShapeSamples` did not include `grad.first` and `grad.last` - `make_arbitrary_grad` was not available as `pp.make_arbitrary_grad` - `add_gradients`' detection of arbitrary gradients was incorrect for Python's 0-based indexing - `add_gradients` handling of non-unique times was broken - `make_trapezoid` incorrectly checked and reported the minimum duration for gradients specifying area and duration (also fixed in local upstream MATLAB pulseq) - `rotate` did not have a system parameter - `rotate` used the incorrect channel for gradient on the second rotation axis
    - Small change in `add_gradients` to accept a single gradient (i.e. for cases where a variable number of gradients are added), which also allowed a minor cleanup in `rotate`

--- pypulseq/Sequence/sequence.py
+++ pypulseq/Sequence/sequence.py
@@ -1392,13 +1392,13 @@ class Sequence:
                             # TODO: Implement restoreAdditionalShapeSamples
                             #       https://github.com/pulseq/pulseq/blob/master/matlab/%2Bmr/restoreAdditionalShapeSamples.m
                             
-                            out_len[j] += len(grad.tt)
-                            shape_pieces[j].append(np.array(
+                            out_len[j] += len(grad.tt)+2
+                            shape_pieces[j, block_counter] = np.array(
                                 [
-                                    curr_dur + grad.delay + grad.tt,
-                                    grad.waveform,
+                                    curr_dur + grad.delay + np.concatenate(([0], grad.tt, [grad.tt[-1] + self.grad_raster_time/2])),
+                                    np.concatenate(([grad.first], grad.waveform, [grad.last]))
                                 ]
-                            ))
+                            )
                         else:  # Extended trapezoid
                             out_len[j] += len(grad.tt)
                             shape_pieces[j].append(np.array(
@@ -1509,7 +1509,6 @@ class Sequence:
 
             wave_data.append(np.concatenate(shape_pieces[j], axis=1))
 
-        for j in range(shape_channels):
             rftdiff = np.diff(wave_data[j][0])
             if np.any(rftdiff < eps):
                 raise Warning(
