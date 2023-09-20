141d8ecf263fa758796195cf516f74c78f9f0d61
Frank Zijlstra <22915457+FrankZijlstra@users.noreply.github.com>
2023-09-20 11:45:27 +0200

    reverted calc_duration

--- pypulseq/calc_duration.py
+++ pypulseq/calc_duration.py
@@ -20,16 +20,15 @@ def calc_duration(*args: SimpleNamespace) -> float:
         Cumulative duration of `args`.
     """
 
+    events = block_to_events(*args)
+
     duration = 0
-    for event in args:
+    for event in events:
         if isinstance(event, (float, int)):  # block_duration field
             assert duration <= event
             duration = event
             continue
         
-        if isinstance(event, dict): # Label field, ignore
-            continue
-
         if not isinstance(event, (dict, SimpleNamespace)):
             raise TypeError(
                 "input(s) should be of type SimpleNamespace or a dict() in case of LABELINC or LABELSET"
